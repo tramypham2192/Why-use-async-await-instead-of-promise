52a21a4b91d223c3f3af3c073cd42a9f176be950
tramypham2192 <61374685+tramypham2192@users.noreply.github.com>
2022-07-02 16:35:07 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const brainstormDinner = require('./library.js');
+
+
+// Native promise version:
+function nativePromiseDinner() {
+  brainstormDinner().then((meal) => {
+	  console.log(`I'm going to make ${meal} for dinner.`);
+  });
+}
+
+nativePromiseDinner()
+
+// async/await version:
+async function announceDinner() {
+  // Write your code below:
+  let resolvedValue = await brainstormDinner();
+  console.log(`I'm going to make ${resolvedValue} for dinner.`);
+}
+
+announceDinner()
